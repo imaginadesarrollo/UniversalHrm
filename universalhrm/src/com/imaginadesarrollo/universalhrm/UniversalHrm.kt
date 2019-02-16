61326990146ac287456d653a5907d413f95d1fc5
Imagina desarrollo <imaginadesarrollo1@gmail.com>
2019-02-16 13:01:24 +0100

    Add callback on "interface"

--- universalhrm/src/com/imaginadesarrollo/universalhrm/UniversalHrm.kt
+++ universalhrm/src/com/imaginadesarrollo/universalhrm/UniversalHrm.kt
@@ -1,11 +1,12 @@
 package com.imaginadesarrollo.universalhrm
 
+import android.support.v7.app.AppCompatActivity
 import com.imaginadesarrollo.universalhrm.main.HrmImplementation
 import com.imaginadesarrollo.universalhrm.main.UniversalHrmImplementation
 
-class UniversalHrm(activity: android.support.v7.app.AppCompatActivity): HrmImplementation {
+class UniversalHrm(activity: AppCompatActivity, callbackMethods: HrmCallbackMethods? = null): HrmImplementation {
 
-    private val universalHrmImplementatiom = UniversalHrmImplementation(activity)
+    private val universalHrmImplementatiom = UniversalHrmImplementation(activity, callbackMethods)
 
     override fun scan() = universalHrmImplementatiom.scan()
 
